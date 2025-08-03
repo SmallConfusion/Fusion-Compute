6ebd5ab02812c016b28598ecd696e9f1039474ee
SmallConfusion <smallconfusion@protonmail.com>
2025-08-03 13:56:15 -0500

    Revert "Tweak build script"
    This reverts commit e331c0755bc24b23147087bfa167f06cf8765b2d.

--- build.py
+++ build.py
@@ -1,13 +1,13 @@
 import zipfile
 import os
 
-build_file_name = "fusion-compute-plugin"
+build_file_name = "fusion-compute-plugin.zip"
 
 compress_type = zipfile.ZIP_DEFLATED
 compress_level = 9
 
 with zipfile.ZipFile(
-			build_file_name + ".zip",
+			build_file_name,
 			"w",
 			compress_type,
 			compresslevel=compress_level
@@ -16,6 +16,6 @@ with zipfile.ZipFile(
 	for root, dirs, files in os.walk("addons"):
 		for file in files:
 			filepath = os.path.join(root, file)
-			zf.write(filepath, os.path.join(build_file_name, filepath))
+			zf.write(filepath, filepath)
 	
-	zf.write("README.md", os.path.join(build_file_name, "addons", "fusion_compute", "README.md"))
+	zf.write("README.md", os.path.join("addons", "fusion_compute", "README.md"))
